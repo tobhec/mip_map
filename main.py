7440726b022f4a587e59901a3b25b70cd97d5d77
Tobias Hector <tobias.hector@hotmail.com>
2025-04-20 22:59:32 +0200

    Added combination of all datasets into one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import data_loading as dl
+import pandas as pd
 
 
 ## SET OUTPUT FOLDER
@@ -11,65 +12,77 @@ country_list = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK",
                 "PT", "RO", "SK", "SI", "ES", "SE"]
 
 #### CURRENT ACCOUNT ####
-df = dl.get_ca(country_list)
-df.to_csv(f"{output_folder}ca_data.csv", index = False)
+ca = dl.get_ca(country_list)
+ca.to_csv(f"{output_folder}ca_data.csv", index = False)
 print("CA data saved to:", f"{output_folder}ca_data.csv \n")
 
 #### NET INTERNATIONAL INVESTMENT POSITION ####
-df = dl.get_niip(country_list)
-df.to_csv(f"{output_folder}niip_data.csv", index = False)
+niip = dl.get_niip(country_list)
+niip.to_csv(f"{output_folder}niip_data.csv", index = False)
 print("NIIP data saved to:", f"{output_folder}niip_data.csv \n")
 
 #### REAL EFFECTIVE EXCHANGE RATE ####
-df = dl.get_reer(country_list)
-df.to_csv(f"{output_folder}reer_data.csv", index = False)
+reer = dl.get_reer(country_list)
+reer.to_csv(f"{output_folder}reer_data.csv", index = False)
 print("REER data saved to:", f"{output_folder}reer_data.csv \n")
 
 #### EXPORT PERFORMANCE AGAINST ADVANCED ECONOMIES ####
-df = dl.get_epaae(country_list)
-df.to_csv(f"{output_folder}nulc_data.csv", index = False)
+epaae = dl.get_epaae(country_list)
+epaae.to_csv(f"{output_folder}epaae_data.csv", index = False)
+print("NULC data saved to:", f"{output_folder}epaae_data.csv \n")
+
+#### NOMINAL UNIT LABOUR COST ####
+nulc = dl.get_nulc(country_list)
+nulc.to_csv(f"{output_folder}nulc_data.csv", index = False)
 print("NULC data saved to:", f"{output_folder}nulc_data.csv \n")
 
 #### GENERAL GOVERNMENT GROSS DEBT ####
-df = dl.get_gggd(country_list)
-df.to_csv(f"{output_folder}gggd_data.csv", index = False)
+gggd = dl.get_gggd(country_list)
+gggd.to_csv(f"{output_folder}gggd_data.csv", index = False)
 print("GGGD data saved to:", f"{output_folder}gggd_data.csv \n")
 
 #### HOUSEHOLD DEBT ####
-df = dl.get_hhd(country_list)
-df.to_csv(f"{output_folder}hhd_data.csv", index = False)
+hhd = dl.get_hhd(country_list)
+hhd.to_csv(f"{output_folder}hhd_data.csv", index = False)
 print("HHD data saved to:", f"{output_folder}hhd_data.csv \n")
 
 #### NON-FINANCIAL CORPORATIONS DEBT ####
-df = dl.get_nfcd(country_list)
-df.to_csv(f"{output_folder}nfcd_data.csv", index = False)
+nfcd = dl.get_nfcd(country_list)
+nfcd.to_csv(f"{output_folder}nfcd_data.csv", index = False)
 print("NFCD data saved to:", f"{output_folder}nfcd_data.csv \n")
 
 #### HOUSEHOLD CREDIT FLOW ####
-df = dl.get_hhcf(country_list)
-df.to_csv(f"{output_folder}hhcf_data.csv", index = False)
+hhcf = dl.get_hhcf(country_list)
+hhcf.to_csv(f"{output_folder}hhcf_data.csv", index = False)
 print("HHCF data saved to:", f"{output_folder}hhcf_data.csv \n")
 
 #### NON-FINANCIAL CORPORATIONS CREDIT FLOW EXCLUDING FDI ####
-df = dl.get_nfccf(country_list)
-df.to_csv(f"{output_folder}nfccf_data.csv", index = False)
+nfccf = dl.get_nfccf(country_list)
+nfccf.to_csv(f"{output_folder}nfccf_data.csv", index = False)
 print("NFCCF data saved to:", f"{output_folder}nfccf_data.csv \n")
 
 #### NOMINAL HOUSE PRICE INDEX ####
-df = dl.get_nhpi(country_list)
-df.to_csv(f"{output_folder}nhpi_data.csv", index = False)
+nhpi = dl.get_nhpi(country_list)
+nhpi.to_csv(f"{output_folder}nhpi_data.csv", index = False)
 print("NHPI data saved to:", f"{output_folder}nhpi_data.csv \n")
 
 #### UNEMPLOYMENT RATE ####
-df = dl.get_unem(country_list)
-df.to_csv(f"{output_folder}unem_data.csv", index = False)
+unem = dl.get_unem(country_list)
+unem.to_csv(f"{output_folder}unem_data.csv", index = False)
 print("UNEM data saved to:", f"{output_folder}unem_data.csv \n")
 
 #### LABOUR FORCE PARTICIPATION RATE ####
-df = dl.get_lfpr(country_list)
-df.to_csv(f"{output_folder}lfpr_data.csv", index = False)
+lfpr = dl.get_lfpr(country_list)
+lfpr.to_csv(f"{output_folder}lfpr_data.csv", index = False)
 print("LFPR data saved to:", f"{output_folder}lfpr_data.csv \n")
 
+tot = pd.concat([ca, niip, reer, epaae, 
+                      nulc, gggd, hhd, nfcd, 
+                      hhcf, nfccf, nhpi, unem, 
+                      lfpr], ignore_index = True)
+tot.to_csv(f"{output_folder}lfpr_data.csv", index = False)
+print(tot)
+print("Total Scoreboard data saved to:", f"{output_folder}mip_sb_data.csv \n")
 
 
 
