0ffd8fa9195d1991314cf33de1a7ec3397b7636c
Tobias Hector <tobias.hector@hotmail.com>
2025-04-20 22:19:45 +0200

    Abstracted functionality into data_loading

--- data_loading.py
+++ data_loading.py
@@ -28,8 +28,350 @@ def get_df(url):
             # Get values
             value = obs.find("{http://www.sdmx.org/resources/sdmxml/schemas/v2_1/data/generic}ObsValue").attrib['value']
             # Add geo, years and values to the dataset
-            records.append({"Year": int(year), "Value": float(value), "Country": geo})
+            records.append({"Year": int(year), "Country": geo, "Value": float(value)})
     df = pd.DataFrame(records)
     return df
 
 
+def construct_url(base, dataset, dim_list, countries):
+    dims = ""
+    for dim in dim_list:
+        dims = dims + dim + "."
+    geo = ""
+    for cc in countries:
+        geo = geo + cc + "+"
+    full_url = f"{base}/{dataset}/{dims}{geo[:-1]}"
+    return full_url
+
+import data_loading as dl
+
+
+def get_ca(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    dataset = "TIPSBP10"
+    freq = "A"
+    unit = "PC_GDP_3Y"
+    s_adj = "NSA"
+    bop_item = "CA"
+    stk_flow = "BAL"
+    partner = "WRL_REST"
+    dim_list = [freq, unit, s_adj, bop_item, stk_flow, partner]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting CA data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "Current account"
+    print(df)
+    return(df)
+
+def get_niip(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    dataset = "TIPSII10"
+    freq = "A"
+    s_adj = "NSA"
+    bop_item = "FA"
+    sector10 = "S1"
+    sectpart = "S1"
+    stk_flow = "N_LE"
+    partner = "WRL_REST"
+    unit = "PC_GDP"
+    dim_list = [freq, s_adj, bop_item, sector10, sectpart, stk_flow, partner, unit]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting NIIP data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "NIIP"
+    print(df)
+    return(df)
+
+def get_reer(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSER10"
+    freq = "A"
+    unit = "PCH_3Y"
+    dim_list = [freq, unit]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting REER data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "REER"
+    print(df)
+    return(df)
+
+
+def get_epaae(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSBP60"
+    freq = "A"
+    unit = "PCH_OECD_EU_3Y"
+    bop_item = "GS"
+    stk_flow = "CRE"
+    partner = "WRL_REST"
+    dim_list = [freq, unit, bop_item, stk_flow, partner]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting EPAAE data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "EPAAE"
+    print(df)
+    return(df)
+
+
+def get_nulc(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSLM10"
+    freq = "A"
+    na_item = "NULC_HW"
+    unit = "PCH_3Y"
+    dim_list = [freq, na_item, unit]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting NULC data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "NULC"
+    print(df)
+    return(df)
+
+
+def get_gggd(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSGO10"
+    freq = "A"
+    na_item = "GD"
+    sector = "S13"
+    unit = "PC_GDP"
+    dim_list = [freq, na_item, sector, unit]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting GGGD data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "GGGD"
+    print(df)
+    return(df)
+
+
+def get_hhd(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSPD22"
+    freq = "A"
+    unit = "PC_GDP"
+    co_nco = "CO"
+    sector = "S14_S15"
+    finpos = "LIAB"
+    na_item = "F3_F4"
+    dim_list = [freq, unit, co_nco, sector, finpos, na_item]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting HHD data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "HHD"
+    print(df)
+    return(df)
+
+
+def get_nfcd(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSPD30"
+    freq = "A"
+    unit = "PC_GDP"
+    co_nco = "CO"
+    sector = "S11"
+    finpos = "LIAB"
+    na_item = "F3_F4"
+    dim_list = [freq, unit, co_nco, sector, finpos, na_item]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting NFCD data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "NFCD"
+    print(df)
+    return(df)
+
+
+def get_hhcf(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSPC40"
+    freq = "A"
+    na_item = "F3_F4"
+    co_nco = "CO"
+    sector = "S14_S15"
+    finpos = "LIAB"
+    unit = "PC_LE"
+    dim_list = [freq, na_item, co_nco, sector, finpos, unit]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting HHCF data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "HHCF"
+    print(df)
+    return(df)
+
+
+def get_nfccf(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSPC30"
+    freq = "A"
+    unit = "PC_LE"
+    co_nco = "CO"
+    sector = "S11"
+    finpos = "LIAB"
+    na_item = "F3_F4_X_FDI"
+    dim_list = [freq, unit, co_nco, sector, finpos, na_item]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting NFCCF data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "NFCCF"
+    print(df)
+    return(df)
+
+
+def get_nhpi(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSHO20"
+    freq = "A"
+    unit = "RCH_A_AVG"
+    dim_list = freq, unit
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting NHPI data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "NHPI"
+    print(df)
+    return(df)
+
+
+def get_unem(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSUN20"
+    freq = "A"
+    sex = "T"
+    age = "Y15-74"
+    unit = "PC_ACT"
+    dim_list = [freq, sex, age, unit]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting UNEM data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "UNEM"
+    print(df)
+    return(df)
+
+
+def get_lfpr(country_list):
+    ## Set base URL
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+
+    ## Define dimensions
+    base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
+    dataset = "TIPSLM60"
+    freq = "A"
+    unit = "PPCH_3Y"
+    age = "Y15-64"
+    sex = "T"
+    dim_list = [freq, unit, age, sex]
+
+    # Get url
+    full_url = construct_url(base_url, dataset, dim_list, country_list)
+    print("Getting LFPR data!")
+    print(full_url)
+
+    # Load data
+    df = get_df(full_url)
+    df["Indicator"] = "LFPR"
+    print(df)
+    return(df)
--- main.py
+++ main.py
@@ -1,347 +1,72 @@
-from data_loading import get_df
-import pandas as pd
+import data_loading as dl
 
-## CREATE LIST WITH COUNTRIES
+
+## SET OUTPUT FOLDER
+output_folder = "C://Users/Tobia/mip_map/output_folder/"
+
+## SET COUNTRY LIST
 country_list = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", 
                 "EE", "FI", "FR", "DE", "EL", "HU", "IE", 
                 "IT", "LV", "LT", "LU", "MT", "NL", "PL", 
                 "PT", "RO", "SK", "SI", "ES", "SE"]
 
-## CREATE URL
-#base_url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nama_10_gdp?format=JSON&lang=EN&geo=SE&unit=CP_MEUR&na_item=P6"
-#base_url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nama_10_gdp?format=JSON&lang=EN&"
-output_folder = "C://Users/Tobia/mip_map/output_folder/"
-
 #### CURRENT ACCOUNT ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSBP10"
-freq = "A"
-unit = "PC_GDP_3Y"
-s_adj = "NSA"
-bop_item = "CA"
-stk_flow = "BAL"
-partner = "WRL_REST"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{s_adj}.{bop_item}.{stk_flow}.{partner}.{geo[:-1]}"
-print("Getting CA data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "Current account"
-print(df)
-
-# Save to CSV
+df = dl.get_ca(country_list)
 df.to_csv(f"{output_folder}ca_data.csv", index = False)
 print("CA data saved to:", f"{output_folder}ca_data.csv \n")
 
 #### NET INTERNATIONAL INVESTMENT POSITION ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSII10"
-freq = "A"
-s_adj = "NSA"
-bop_item = "FA"
-sector10 = "S1"
-sectpart = "S1"
-stk_flow = "N_LE"
-partner = "WRL_REST"
-unit = "PC_GDP"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{s_adj}.{bop_item}.{sector10}.{sectpart}.{stk_flow}.{partner}.{unit}.{geo[:-1]}"
-print("Getting NIIP data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "NIIP"
-print(df)
-
-# Save to CSV
+df = dl.get_niip(country_list)
 df.to_csv(f"{output_folder}niip_data.csv", index = False)
 print("NIIP data saved to:", f"{output_folder}niip_data.csv \n")
 
-
 #### REAL EFFECTIVE EXCHANGE RATE ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSER10"
-freq = "A"
-unit = "PCH_3Y"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{geo[:-1]}"
-print("Getting REER data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "REER"
-print(df)
-
-# Save to CSV
+df = dl.get_reer(country_list)
 df.to_csv(f"{output_folder}reer_data.csv", index = False)
 print("REER data saved to:", f"{output_folder}reer_data.csv \n")
 
-
 #### EXPORT PERFORMANCE AGAINST ADVANCED ECONOMIES ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSBP60"
-freq = "A"
-unit = "PCH_OECD_EU_3Y"
-bop_item = "GS"
-stk_flow = "CRE"
-partner = "WRL_REST"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{bop_item}.{stk_flow}.{partner}.{geo[:-1]}"
-print("Getting EPAAE data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "EPAAE"
-print(df)
-
-# Save to CSV
-df.to_csv(f"{output_folder}epaae_data.csv", index = False)
-print("EPAAE data saved to:", f"{output_folder}epaae_data.csv \n")
-
-
-#### NOMINAL UNIT LABOUR COST ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSLM10"
-freq = "A"
-na_item = "NULC_HW"
-unit = "PCH_3Y"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{na_item}.{unit}.{geo[:-1]}"
-print("Getting NULC data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "NULC"
-print(df)
-
-# Save to CSV
+df = dl.get_epaae(country_list)
 df.to_csv(f"{output_folder}nulc_data.csv", index = False)
 print("NULC data saved to:", f"{output_folder}nulc_data.csv \n")
 
-
 #### GENERAL GOVERNMENT GROSS DEBT ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSGO10"
-freq = "A"
-na_item = "GD"
-sector = "S13"
-unit = "PC_GDP"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{na_item}.{sector}.{unit}.{geo[:-1]}"
-print("Getting GGGD data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "GGGD"
-print(df)
-
-# Save to CSV
+df = dl.get_gggd(country_list)
 df.to_csv(f"{output_folder}gggd_data.csv", index = False)
 print("GGGD data saved to:", f"{output_folder}gggd_data.csv \n")
 
-
 #### HOUSEHOLD DEBT ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSPD22"
-freq = "A"
-unit = "PC_GDP"
-co_nco = "CO"
-sector = "S14_S15"
-finpos = "LIAB"
-na_item = "F3_F4"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{co_nco}.{sector}.{finpos}.{na_item}.{geo[:-1]}"
-print("Getting HHD data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "HHD"
-print(df)
-
-# Save to CSV
+df = dl.get_hhd(country_list)
 df.to_csv(f"{output_folder}hhd_data.csv", index = False)
 print("HHD data saved to:", f"{output_folder}hhd_data.csv \n")
 
-
 #### NON-FINANCIAL CORPORATIONS DEBT ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSPD30"
-freq = "A"
-unit = "PC_GDP"
-co_nco = "CO"
-sector = "S11"
-finpos = "LIAB"
-na_item = "F3_F4"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{co_nco}.{sector}.{finpos}.{na_item}.{geo[:-1]}"
-print("Getting NFCD data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "NFCD"
-print(df)
-
-# Save to CSV
+df = dl.get_nfcd(country_list)
 df.to_csv(f"{output_folder}nfcd_data.csv", index = False)
 print("NFCD data saved to:", f"{output_folder}nfcd_data.csv \n")
 
-
 #### HOUSEHOLD CREDIT FLOW ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSPC40"
-freq = "A"
-na_item = "F3_F4"
-co_nco = "CO"
-sector = "S14_S15"
-finpos = "LIAB"
-unit = "PC_LE"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{na_item}.{co_nco}.{sector}.{finpos}.{unit}.{geo[:-1]}"
-print("Getting HHCF data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "HHCF"
-print(df)
-
-# Save to CSV
+df = dl.get_hhcf(country_list)
 df.to_csv(f"{output_folder}hhcf_data.csv", index = False)
 print("HHCF data saved to:", f"{output_folder}hhcf_data.csv \n")
 
-
 #### NON-FINANCIAL CORPORATIONS CREDIT FLOW EXCLUDING FDI ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSPC30"
-freq = "A"
-unit = "PC_LE"
-co_nco = "CO"
-sector = "S11"
-finpos = "LIAB"
-na_item = "F3_F4_X_FDI"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{co_nco}.{sector}.{finpos}.{na_item}.{geo[:-1]}"
-print("Getting NFCCF data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "NFCCF"
-print(df)
-
-# Save to CSV
+df = dl.get_nfccf(country_list)
 df.to_csv(f"{output_folder}nfccf_data.csv", index = False)
 print("NFCCF data saved to:", f"{output_folder}nfccf_data.csv \n")
 
-
 #### NOMINAL HOUSE PRICE INDEX ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSHO20"
-freq = "A"
-unit = "RCH_A_AVG"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{geo[:-1]}"
-print("Getting NHPI data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "NHPI"
-print(df)
-
-# Save to CSV
+df = dl.get_nhpi(country_list)
 df.to_csv(f"{output_folder}nhpi_data.csv", index = False)
 print("NHPI data saved to:", f"{output_folder}nhpi_data.csv \n")
 
-
 #### UNEMPLOYMENT RATE ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSUN20"
-freq = "A"
-sex = "T"
-age = "Y15-74"
-unit = "PC_ACT"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{sex}.{age}.{unit}.{geo[:-1]}"
-print("Getting UNEM data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "UNEM"
-print(df)
-
-# Save to CSV
+df = dl.get_unem(country_list)
 df.to_csv(f"{output_folder}unem_data.csv", index = False)
 print("UNEM data saved to:", f"{output_folder}unem_data.csv \n")
 
-
 #### LABOUR FORCE PARTICIPATION RATE ####
-## SDMX URL
-base_url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data"
-dataset = "TIPSLM60"
-freq = "A"
-unit = "PPCH_3Y"
-age = "Y15-64"
-sex = "T"
-geo = ""
-for country in country_list:
-    geo = geo + country + "+"
-full_url = base_url + f"/{dataset}/{freq}.{unit}.{age}.{sex}.{geo[:-1]}"
-print("Getting LFPR data!")
-print(full_url)
-
-# Load data
-df = get_df(full_url)
-df["Indicator"] = "LFPR"
-print(df)
-
-# Save to CSV
+df = dl.get_lfpr(country_list)
 df.to_csv(f"{output_folder}lfpr_data.csv", index = False)
 print("LFPR data saved to:", f"{output_folder}lfpr_data.csv \n")
 
