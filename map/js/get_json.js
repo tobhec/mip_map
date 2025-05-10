0f9a439ad6c531699ed5f39a6c2edd165ed85ee6
Tobias Hector <tobias.hector@hotmail.com>
2025-05-10 12:36:40 +0200

    Moved the eventdispact into a promiseall

--- map/js/get_json.js
+++ map/js/get_json.js
@@ -1,7 +1,20 @@
-fetch('../../data/output/scale_dict.json')
-  .then(response => response.json())
-  .then(colour_thresholds => {
-    document.dispatchEvent(new CustomEvent("configLoading", {detail: colour_thresholds}));
-    console.log("Step 1");
+function loadConfigFile(path) {
+  return fetch(path)
+    .then(response => response.json());
+}
+  
+Promise.all([loadConfigFile("../../data/output/scale_dict.json")])
+  .then(([scale_dict]) => {
+    const config = {
+      scale_dict: scale_dict
+    };
+
+    const event = new CustomEvent('configLoading', {
+      detail: config
+    });
+
+    document.dispatchEvent(event);
   })
-  .catch(error => console.error('Error:', error));
+  .catch(error => {
+    console.error('Failed to load config:', error);
+});
