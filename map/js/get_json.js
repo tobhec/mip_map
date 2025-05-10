915be59ddffef5414bbe5b166d444f474f245331
Tobias Hector <tobias.hector@hotmail.com>
2025-05-10 12:52:06 +0200

    Added dynamic unit loading

--- map/js/get_json.js
+++ map/js/get_json.js
@@ -4,12 +4,14 @@ function loadConfigFile(path) {
 }
   
 Promise.all([loadConfigFile("../../data/config/scale_dict.json"),
-             loadConfigFile("../../data/config/threshold_dict.json")
+             loadConfigFile("../../data/config/threshold_dict.json"),
+             loadConfigFile("../../data/config/unit_dict.json")
 ])
-  .then(([scale_dict, threshold_dict]) => {
+  .then(([scale_dict, threshold_dict, unit_dict]) => {
     const config = {
       scale_dict: scale_dict,
-      threshold_dict: threshold_dict
+      threshold_dict: threshold_dict,
+      unit_dict: unit_dict
     };
 
     const event = new CustomEvent('configLoading', {
